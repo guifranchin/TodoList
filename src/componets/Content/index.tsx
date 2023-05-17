875b21e18bbafb0d3e5c815409e0399cd734e44f
Guilherme <gui.franchim@gmail.com>
2023-05-16 21:37:40 -0300

    feat: adding server-json

--- src/componets/Content/index.tsx
+++ src/componets/Content/index.tsx
@@ -1,48 +1,64 @@
 import style from "./index.module.css";
 import Plus from "../../assets/plus.svg";
 import { NoContent } from "../NoContent";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TodoList } from "../TodoList";
 import { Task } from "../../models/Task";
-
+import { v4 as uuidv4 } from "uuid";
+import { api } from "../../api";
 export const Content = () => {
   const [description, setDescription] = useState<string>("");
 
-  const [taskList, setTaskList] = useState<Task[]>([
-    {
-      id: "1",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum interdum imperdiet.",
-      isDone: false,
-    },
-    {
-      id: "2",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum interdum imperdiet.",
-      isDone: false,
-    },
-    {
-      id: "3",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum interdum imperdiet.",
-      isDone: true,
-    },
-  ]);
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  const tasksDone = taskList.filter((task) => {
+    return task.isDone !== false;
+  });
+
+  const disabledButton = !description.length;
 
   const addTaskOnList = () => {
     const newTask = {
-      id: "20",
+      id: uuidv4(),
       description,
       isDone: false,
     };
 
     setTaskList((currentValue) => [...currentValue, newTask]);
+    setDescription("");
   };
 
-
   const removeTaskOnDelete = (id: string) => {
-    setTaskList((currentValue) => currentValue.filter(task => task.id !== id))
-  }
+    setTaskList((currentValue) =>
+      currentValue.filter((task) => task.id !== id)
+    );
+  };
+
+  const changeStatusCheckBox = (id: string) => {
+    const elements = taskList.map((task) => {
+      if (task.id == id) {
+        return {
+          ...task,
+          isDone: !task.isDone,
+        };
+      }
+
+      return task;
+    });
+
+    setTaskList(elements);
+  };
+
+  useEffect(() => {
+    api
+      .get("tasks")
+      .then((response) => {
+        return response.data;
+      })
+      .then((data) => {
+        setTaskList(data);
+      });
+  }, []);
 
   return (
     <section className={style.section_container}>
@@ -51,11 +67,16 @@ export const Content = () => {
           <input
             className={style.input}
             type="text"
+            value={description}
             placeholder="Adicione uma nova tarefa"
             onChange={(e) => setDescription(e.target.value)}
           ></input>
           <article>
-            <button className={style.button} onClick={() => addTaskOnList()}>
+            <button
+              className={style.button}
+              disabled={disabledButton}
+              onClick={() => addTaskOnList()}
+            >
               Criar
               <img src={Plus} alt="Logo de mais"></img>
             </button>
@@ -64,15 +85,26 @@ export const Content = () => {
         <article className={style.content_header}>
           <article className={style.tasks_container}>
             <p className={style.task_created}>Tarefas Criadas</p>
-            <span className={style.span_value}>0</span>
+            <span className={style.span_value}>{taskList.length}</span>
           </article>
           <article className={style.tasks_container}>
             <p className={style.tasks_done}>Concluidas</p>
-            <span className={style.span_value}>0</span>
+            <span className={style.span_value}>
+              {" "}
+              {tasksDone.length} de {taskList.length}
+            </span>
           </article>
         </article>
 
-        {!taskList.length ? <NoContent /> : <TodoList list={taskList} onDelete={removeTaskOnDelete}/>}
+        {!taskList.length ? (
+          <NoContent />
+        ) : (
+          <TodoList
+            list={taskList}
+            changeStatusCheckBox={changeStatusCheckBox}
+            onDelete={removeTaskOnDelete}
+          />
+        )}
       </main>
     </section>
   );
--- src/componets/TodoList/index.tsx
+++ src/componets/TodoList/index.tsx
@@ -4,16 +4,17 @@ import { Task } from "../../models/Task";
 
 interface TodoListProps {
   list: Task[];
-  onDelete: (id: string) => void
+  onDelete: (id: string) => void,
+  changeStatusCheckBox: (id: string) => void
 }
 
-export const TodoList = ({ list, onDelete }: TodoListProps) => {
+export const TodoList = ({ list, onDelete, changeStatusCheckBox }: TodoListProps) => {
   return (
     <section className={styles.section_container}>
       {list.map((task) => (
         <article className={styles.content_container} id={task.id}>
-          <input type="checkbox" id={task.id} defaultChecked={task.isDone} />
-          <p className={styles.text}>{task.description}</p>
+          <input type="checkbox" id={task.id} defaultChecked={task.isDone} onChange={() => changeStatusCheckBox(task.id)} />
+          <p className={task.isDone ? styles.text_scratched : styles.text}>{task.description}</p>
           <img className={styles.trash} src={Trash} alt="icone de lixeira" onClick={() => onDelete(task.id)}/>
         </article>
       ))}
