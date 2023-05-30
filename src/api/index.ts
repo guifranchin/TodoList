63030f7499c7abc71e191d95c98812b08764218a
Guilherme <gui.franchim@gmail.com>
2023-05-30 17:56:06 -0300

    feat/adding jsonServer

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333/',
-    
+    baseURL: 'https://api.jsonserver.io/',
+    headers: {
+        'X-Jsio-Token': '31067ca705373bf18d7c3e1277583b3b'
+    }
 });
