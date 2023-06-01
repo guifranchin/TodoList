d45ef2d9bc83e536a979f043fa5435777cf89432
Guilherme <gui.franchim@gmail.com>
2023-06-01 20:35:18 -0300

    change: changing baseUrl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api.jsonserver.io/",
-  headers: { "X-Jsio-Token": "31067ca705373bf18d7c3e1277583b3b" },
+  baseURL: "https://json-server-e529.onrender.com/",
+  // headers: { "X-Jsio-Token": "31067ca705373bf18d7c3e1277583b3b" },
 });
+
+export const apiRequest = async <T> () =>{
+
+}
